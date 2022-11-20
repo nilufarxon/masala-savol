1667475d2f94c41eb141ad338d2024aac32bc363
nilufarxon <orifjonova07@inbox.ru>
2022-11-20 16:59:11 +0500

    masala&savol

---
+++ masala/app.js
@@ -0,0 +1,17 @@
+//1- Js dan foydalanib 8 ning 5 darajasi neciga teng ekanligini ayting.
+//2- 1 dan 10 gacha tasodifiy sonni qanday yaratish mumkin?
+//3-typeof da null nima?
+//4- console.log(NaN ** 2) nima chiqadi
+//5-   0 || 1 && 2 || 3 nma chiqadi va nima uchun bunday javob chiqdi?
+///Javoblari
+// 1
+let m = 8;
+console.log(Math.pow(m, 5)); //32768
+//2
+console.log(Math.floor(Math.random() * 10 + 1));
+//3
+console.log(typeof null);
+//4
+console.log(NaN ** 2); //NaN
+//5
+console.log(0 || (1 && 2) || 3);
---
+++ savol/app.js
@@ -0,0 +1,16 @@
+//1-Js-da isNaN nima?
+//2-Js global o'zgaruvchilari nima?
+//3-"===" qat'iy tenglik nima?
+//4-repeat()  ni vazifasi nma?
+//5-Js-da "typeof" dan qanday foydalanish kerak?
+//6-Qaysi operator  1 ta raqamga qo'shadi?
+//7-** operatori nima qiladi?
+
+///Javoblari
+//1 J:sNan  berilgan qiymatning sonli yoki yo'qligini tekshirish uchun ishlatiladi.
+//2 J:Global o'zgaruvchilar - bu kodning butun uzunligi davomida mavjud bo'lgan narsalar, ya'ni ularning doirasi yo'q. Var kalit so'zi mahalliy o'zgaruvchini yoki ob'ektni e'lon qilish uchun ishlatiladi.
+//3 J:" === "Ikkita operand bir xil qiymatga va bir xil ma'lumot turiga ega bo'lganda haqiqiylikni qaytaradigan qat'iy tenglik operatori deb ataladi.
+//4 J:faqat stringni  takrorlaw vazifasini bajaradi. parametir son qabul qiladi
+//5 J:typeof - bu o'zgaruvchining turini tavsifini qaytarish uchun ishlatiladigan operator.
+//6 J: ++
+//7 J: darajaga owiradi
